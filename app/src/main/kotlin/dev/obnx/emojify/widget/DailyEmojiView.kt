7ebfc5e079e377f67e710307ecc15f27bb56a4da
David Battefeld <davidbattefeld@gmail.com>
2025-08-15 13:04:47 +0200

    feat: finalize UI prototype

--- app/src/main/kotlin/dev/obnx/emojify/widget/DailyEmojiView.kt
+++ app/src/main/kotlin/dev/obnx/emojify/widget/DailyEmojiView.kt
@@ -4,24 +4,34 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.components.Scaffold
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
+import androidx.glance.preview.ExperimentalGlancePreviewApi
+import androidx.glance.preview.Preview
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 
 @Composable
 fun DailyEmojiView(emoji: String) {
     Scaffold(
+        backgroundColor = GlanceTheme.colors.primaryContainer,
         titleBar = {
             Row(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = GlanceModifier.padding(8.dp)
+                modifier = GlanceModifier.padding(8.dp).fillMaxWidth()
             ) {
-                Text(text = "Your daily emoji")
+                Text(
+                    text = "Your daily emoji",
+                    style = TextStyle(
+                        color = GlanceTheme.colors.onPrimaryContainer,
+                    )
+                )
             }
         }
     ) {
@@ -29,12 +39,23 @@ fun DailyEmojiView(emoji: String) {
             verticalAlignment = Alignment.CenterVertically,
             modifier = GlanceModifier.fillMaxSize()
         ) {
-            Row(horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = GlanceModifier.fillMaxWidth()
+            ) {
                 Text(
                     text = emoji,
-                    style = TextStyle(fontSize = MaterialTheme.typography.headlineLarge.fontSize)
+                    style = TextStyle(fontSize = MaterialTheme.typography.displayMedium.fontSize),
+                    modifier = GlanceModifier.padding(bottom = 12.dp)
                 )
             }
         }
     }
+}
+
+@OptIn(ExperimentalGlancePreviewApi::class)
+@Preview(widthDp = 200, heightDp = 150)
+@Composable
+fun DailyEmojiViewPreview() {
+    DailyEmojiView(emoji = "😀")
 }
