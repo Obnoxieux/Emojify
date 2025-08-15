ccbdbe90308533799b06fda02e689b75647d9dcd
David Battefeld <davidbattefeld@gmail.com>
2025-08-15 12:53:40 +0200

    build: try glance preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.glance.appwidget)
     implementation(libs.androidx.glance.material3)
+    implementation(libs.androidx.glance.preview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/kotlin/dev/obnx/emojify/widget/EmojifyAppWidget.kt
+++ app/src/main/kotlin/dev/obnx/emojify/widget/EmojifyAppWidget.kt
@@ -41,3 +41,5 @@ class EmojifyAppWidget : GlanceAppWidget() {
         }
     }
 }
+
+
